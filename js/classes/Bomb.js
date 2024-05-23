d85aced2321ac7dc1a336e83e6f53b0cdccfc33b
anavivisilvadias <anavitoriadiasdasilva47@gmail.com>
2024-05-23 11:29:45 -0400

    add draw function

--- js/classes/Bomb.js
+++ js/classes/Bomb.js
@@ -58,9 +58,18 @@ class Bomb {
         });
     }
 }     
-class PowerUp {
+
+    class PowerUp {
     constructor({ position, velocity }) {
         this.position = position;
         this.velocity = velocity;
         this.radius = 15;
+    } 
+
+    draw() {
+        c.beginPath();
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+        c.fillStyle = "yellow";
+        c.fill();
+        c.closePath();
     }
