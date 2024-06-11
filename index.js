1dbda7000b02a56ff828d686d0a9ac3576071a6d
anavivisilvadias <anavitoriadiasdasilva47@gmail.com>
2024-06-11 11:43:03 -0400

    add initial variables

--- index.js
+++ index.js
@@ -4,3 +4,39 @@ const c = canvas.getContext("2d");
 
 canvas.width = 1024;
 canvas.height = 576;
+
+let player = new Player();
+let projectles = [];
+let grids = [];
+let invaderProjectiles = [];
+let particles = [];
+let bombs = [];
+let PowerUp = [];
+
+let keys = {
+    ArrowLeft:{
+        pressed: false
+    },
+    ArrowRight: {
+        pressed: false
+    },
+    space: {
+        pressed: false
+    }
+};
+
+let frames = 0;
+let randomInterval = Math.floor(Math.random() * 500 + 500);
+
+let game = {
+    over: false,
+    active: true 
+};
+
+let score = 0;
+
+let spawnBuffer = 500;
+let fps = 60;
+let fpsInterval = 1000 / fps;
+
+let msPrev = window.performance.now();
