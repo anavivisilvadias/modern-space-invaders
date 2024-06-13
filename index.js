7a1d7969f2b91b689abef3988373bae891a12a7d
anavivisilvadias <anavitoriadiasdasilva47@gmail.com>
2024-06-13 07:41:55 -0400

    add init function in index.js file

--- index.js
+++ index.js
@@ -5,6 +5,43 @@ const c = canvas.getContext("2d");
 canvas.width = 1024;
 canvas.height = 576;
 
+player = new Player();
+projectles = [];
+grids = [];
+invaderProjectiles = [];
+particles = [];
+bombs = [];
+PowerUp = [];
+
+keys = {
+  ArrowLeft: {
+    pressed: false
+  },
+  ArrowRight: {
+    pressed: false
+  },
+  space: {
+    pressed: false
+  }
+};
+
+frames = 0;
+randomInterval = Math.floor(Math.random() * 500 + 500);
+
+game = {
+  over: false,
+  active: true
+};
+
+score = 0;
+
+spawnBuffer = 500;
+fps = 60;
+fpsInterval = 1000 / fps;
+
+msPrev = window.performance.now();
+
+
 let player = new Player();
 let projectles = [];
 let grids = [];
@@ -35,8 +72,21 @@ let game = {
 
 let score = 0;
 
-let spawnBuffer = 500;
-let fps = 60;
-let fpsInterval = 1000 / fps;
+for(let i = 0; i < 100; i++) {
+    particles.push(
+        new Particle({
+            position: {
+                x: Math.random() * canvas.width,
+                y: Math.random() * canvas.height
+            },
+            velocity: {
+                x: 0,
+                y: 0.3
+            },
+            radius: Math.random() * 2,
+            color: "white",
+            fades: true
+        })
+    )
+}
 
-let msPrev = window.performance.now();
