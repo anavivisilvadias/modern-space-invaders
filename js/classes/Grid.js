589206f86971706c0ca0afd51242b07c23a85da8
anavivisilvadias <anavitoriadiasdasilva47@gmail.com>
2024-06-20 08:53:43 -0400

    add player control events and finalize project

--- js/classes/Grid.js
+++ js/classes/Grid.js
@@ -1,83 +1,45 @@
-class Bomb {
-  static radius = 30;
-  constructor({ position, velocity }) {
-    this.position = position;
-    this.velocity = velocity;
-    this.radius = 0;
-    this.color = "red";
-    this.opacity = 1;
-    this.active = false;
+class Grid {
+  constructor() {
+    this.position = {
+      x: 0,
+      y: 0
+    };
 
-    gsap.to(this, {
-      radius: 30
-    });
-  }
+    this.velocity = {
+      x: 3,
+      y: 0
+    };
 
-  draw() {
-    c.save();
-    c.globalAlpha = this.opacity;
-    c.beginPath();
-    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
-    c.closePath();
-    c.fillStyle = this.color;
-    c.fill();
-    c.restore();
+    this.invaders = [];
+
+    const columns = Math.floor(Math.random() * 10 + 5);
+    const rows = Math.floor(Math.random() * 5 + 2);
+
+    this.width = columns * 30;
+
+    for (let x = 0; x < columns; x++) {
+      for (let y = 0; y < rows; y++) {
+        this.invaders.push(
+          new Invader({
+            position: {
+              x: x * 30,
+              y: y * 30
+            }
+          })
+        );
+      }
+    }
   }
 
   update() {
-    this.draw();
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
 
-    if (
-      this.position.x + this.radius + this.velocity.x >= canvas.width ||
-      this.position.x - this.radius + this.velocity.x <= 0
-    ) {
-      this.velocity.x = -this.velocity.x;
-    } else if (
-      this.position.y + this.radius + this.velocity.y >= canvas.height ||
-      this.position.y - this.radius + this.velocity.y <= 0
-    )
-      this.velocity.y = -this.velocity.y;
-  }
-
-  explode() {
-    audio.bomb.play();
-    this.active = true;
-    this.velocity.x = 0;
     this.velocity.y = 0;
 
-    gsap.to(this, {
-      radius: 200,
-      color: "red"
-    });
-
-    gsap.to(this, {
-      delay: 0.1,
-      opacity: 0,
-      duration: 0.15
-    });
-  }
-}
-
-class PowerUp {
-  constructor({ position, velocity }) {
-    this.position = position;
-    this.velocity = velocity;
-    this.radius = 15;
-  }
-
-  draw() {
-    c.beginPath();
-    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
-    c.fillStyle = "yellow";
-    c.fill();
-    c.closePath();
-  }
-
-  update() {
-    this.draw();
-    this.position.x += this.velocity.x;
-    this.position.y += this.velocity.y;
+    if (this.position.x + this.width >= canvas.width || this.position.x <= 0) {
+      this.velocity.x = -this.velocity.x * 1.15;
+      this.velocity.y = 30;
+    }
   }
 }
